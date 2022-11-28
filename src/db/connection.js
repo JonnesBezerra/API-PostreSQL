887e937cb45b258cd182a4c0abed34096363aa6f
Jonnes Bezerra <jonnes.bezerra@gmail.com>
2022-11-28 01:36:08 -0300

    Refact: variables from .env

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,11 +1,12 @@
+require('dotenv').config();
 const Pool = require('pg').Pool;
 
 const connection = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'Library',
-  password: '1243',
-  port: 5432,
+  user: process.env.POSTGRES_USER,
+  host: process.env.HOSTNAME,
+  database: process.env.POSTGRES_DATABASE,
+  password: process.env.POSTGRES_PASWORD,
+  port: process.env.POSTGRES_PORT,
 });
 
 module.exports = connection;
